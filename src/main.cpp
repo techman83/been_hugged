ee2731848119e0e57fbf7d10d237a880c6de2d1c
Leon Wright <lwright@corpcloud.com.au>
2017-01-19 09:15:17 +0800

    It compiles, that's 'working' right

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,28 @@
  *
  */
 
+#define FASTLED_ESP8266_D1_PIN_ORDER
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
+#include <FastLED.h>
 
-/* Init state globals */
-uint8_t PROX = D5;
-volatile bool stuck = false;
+
+#define RANGE_PIN A0
+char GP2D12;
+char a,b;
+unsigned long huggedTime = 0;
+int hugTicks = 0;
+volatile bool hugStuck = false;
+
+// WS2812 / FASTLED Config
+#define NUM_LEDS 2
+#define DATA_PIN  D2
+uint32_t ledPosition = 0;
+volatile boolean hugs = false;
+CRGB leds[NUM_LEDS];    // Define the array of leds
 
 /* MQTT Settings */
 #define BUFFER_SIZE 100
@@ -48,6 +61,36 @@ PubSubClient client(wificlient);
 /**
  * Setup
  */
+void led_startup() {
+  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+  // Scan red, then green, then blue across the LEDs
+  for(int i = 0; i <= NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(200);
+    leds[i] = CRGB::Black;
+    FastLED.show();
+  }
+  for(int i = 0; i <= NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Green;
+    FastLED.show();
+    delay(200);
+    leds[i] = CRGB::Black;
+    FastLED.show();
+  }
+  for(int i = 0; i <= NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Blue;
+    FastLED.show();
+    delay(200);
+    leds[i] = CRGB::Black;
+    FastLED.show();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
@@ -61,8 +104,6 @@ void setup() {
     ESP.restart();
   }
 
-  pinMode(PROX, INPUT);
-
   Serial.println("Proceeding");
 
   // Hostname defaults to esp8266-[ChipID]
@@ -71,7 +112,7 @@ void setup() {
 
   client.setServer(MQTT, 1883);
   client.setCallback(callback);
-  
+
   ArduinoOTA.onStart([]() {
     Serial.println("Start");
   });
@@ -93,6 +134,8 @@ void setup() {
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  led_startup();
+  delay(2000);
 }
 
 void reconnect() {
@@ -117,28 +160,96 @@ void reconnect() {
   }
 }
 
-void is_it_me() { 
-  if(stuck == true)
-    return;  
+float read_gp2d12_range(byte pin)
+{
+  int tmp;
+  tmp = analogRead(pin);
+  if (tmp < 3)return -1;
+  return (13574.0 /((float)tmp - 3.0)) - 4.0;
+}
 
-  int state = digitalRead(PROX);
-  int count = 0;
-  while (count < 30 && state == 0) {
-    if (count == 5) {
-      Serial.println("I've been hugged");
+void is_it_me() {
+  unsigned long timeNow = millis();
+  if (timeNow >= huggedTime && hugStuck == false) {
+    huggedTime = timeNow + 100;
+    int val;
+    int i;
+    GP2D12=read_gp2d12_range(RANGE_PIN);
+    a=GP2D12/10;
+    b=GP2D12%10;
+    val=a*10+b;
+
+    if(val>10 && val<80)
+    {
+      Serial.print(a,DEC);
+      Serial.print(b,DEC);
+      Serial.println("cm");
+    }
+
+    // (Potential)? Hug cleared
+    if (val>30)
+    {
+      Serial.println("(Potential)? Hug cleared");
+      hugs = false;
+      hugTicks = 0;
+      for(i = 0; i <= NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Black;
+        FastLED.show();
+      }
+    }
+
+    // I've been hugged!
+    if (hugTicks == 15)
+    {
+      Serial.println("I've been hugged <3");
       client.publish("/hugged", "true");
     }
-    
-    if (count > 25) {
-      Serial.println("Hrm is this just a long hug??");
-      stuck = true;
+
+    // Oh noes I'm potentially stuck in a hug!
+    // (more likely my badge is obstructed... or my hacky code)
+    if (hugTicks == 100)
+    {
+      CHSV hsv( 0, 255, 255);
+      for(i = 0; i <= NUM_LEDS; i++)
+      {
+        leds[i] = hsv;
+        FastLED.show();
+      }
+      Serial.println("Ack! I'm stuck!");
       client.publish("/stuck", "true");
-      break;
     }
-     
-    state = digitalRead(PROX);
-    count = count + 1;
-    delay(1000);
+
+    if (val>=15 && val<25 )
+    {
+      Serial.println("Potential hug inbound");
+      hugs = true;
+    }
+
+    if(hugs == true && val<15)
+    {
+      Serial.println("Hug tick");
+      hugTicks += 1;
+
+      int saturation;
+      if (hugTicks < 50)
+      {
+        saturation = hugTicks * 5;
+      }
+      else
+      {
+        saturation = 255;
+      }
+
+      CHSV hsv( 160, saturation, 255);
+      for(i = 0; i <= NUM_LEDS; i++)
+      {
+        leds[i] = hsv;
+        FastLED.show();
+      }
+      // As soon as I call FastLED.show hugs reverts to false.
+      hugs = true; // TODO: I don't even know why this is necessary
+    }
   }
 }
 
@@ -165,7 +276,6 @@ void loop() {
       reconnect();
     }
   }
-
 
   is_it_me();
 
