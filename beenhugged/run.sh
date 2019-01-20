19bf48c9c060f73c537e91e9e590e62ea3c53fee
Leon Wright <techman83@gmail.com>
2019-01-20 17:12:18 +1300

    No need to quote variables with JQ, just means they get double quoted *dusts off yak hair*

--- beenhugged/run.sh
+++ beenhugged/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while IFS== read -r key value; do
-	        export "$key"="$value"
-	done < <(aws secretsmanager get-secret-value --secret-id been_hugged|jq --raw-output .SecretString|jq -r '.|to_entries[]| [.key,(.value|@sh)] | join("=")')
+	export "$key"="$value"
+done < <(aws secretsmanager get-secret-value --secret-id been_hugged --region ap-southeast-2|jq --raw-output .SecretString|jq -r '.|to_entries[]| [.key,(.value)] | join("=")')
 
 beenhugged.py
--- mosquitto/run.sh
+++ mosquitto/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo Retrieving Secrets
 while IFS== read -r key value; do
         export "$key"="$value"
-done < <(aws secretsmanager get-secret-value --secret-id been_hugged|jq --raw-output .SecretString|jq -r '.|to_entries[]| [.key,(.value|@sh)] | join("=")')
+done < <(aws secretsmanager get-secret-value --secret-id been_hugged --region ap-southeast-2|jq --raw-output .SecretString|jq -r '.|to_entries[]| [.key,(.value)] | join("=")')
 
+echo Provsioning MQTT
+touch /etc/mosquitto/pwfile
+mosquitto_passwd -b /etc/mosquitto/pwfile $mqtt_user $mqtt_pass
+mosquitto_passwd -b /etc/mosquitto/pwfile $mqtt_badge_user $mqtt_badge_pass
 mosquitto -c /etc/mosquitto/mosquitto.conf
